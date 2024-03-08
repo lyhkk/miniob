bffda5d0da6454f21491d82da5142011704c4aa4
lyhkk <1047677876@qq.com>
2024-03-08 10:30:25 +0800

    ignore files

--- test/perf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lyhkk/miniob/test/perf
-# Build directory: /home/lyhkk/miniob/test/perf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
